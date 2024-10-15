d005c387763ae127e552d3d7dea95d75fa6d936c
a.radzhabov <a.radzhabov@estesis.tech>
2024-10-15 17:42:56 +0700

    alfkjdslkf

--- run_video.py
+++ run_video.py
@@ -1,3 +1,4 @@
+import time
 from pathlib import Path
 import requests
 
@@ -8,9 +9,8 @@ from kolya_color import device, data_transforms_val, padding, best_model_wts
 import cv2
 import sys
 from ultralytics import YOLO
-import time
 
-backend_url = "http://localhost:5000/track"
+backend_url = "http://localhost:5321/upload_frame/afros_test"
 
 detection = YOLO('yolov8-n-31.05.24.pt')
 
@@ -31,7 +31,7 @@ map_dict = {
     13: 'black',
     14: 'yellow_str',
     15: 'cue_ball',
- }
+}
 
 color_store = {}
 
@@ -60,72 +60,46 @@ def color_identification(video_path):
     new_frame_time = 0
     frame_count = 0
     prevFrame = None
-    max_frame_count = 10000
+    frame_skip = 2
     while cap.isOpened():
         success, frame = cap.read()
         if not success:
             break
-
-        if frame_count >= max_frame_count:
-            break
-        new_frame_time = time.time() 
-        fps = 1/(new_frame_time - prev_frame_time) 
+        new_frame_time = time.time()
+        fps = 1 / (new_frame_time - prev_frame_time)
         prev_frame_time = new_frame_time
         fps = str(int(fps))
-        frame_skip = 8
 
-        results = detection(frame, classes=[0], conf=0.6)
+        # Encode the frame as JPEG
+        _, encoded_image = cv2.imencode('.jpg', frame)
 
-        if len(results) == 0:
-            continue
+        # Convert the encoded image to bytes
+        frame_bytes = encoded_image.tobytes()
 
-        result = results[0]
-        boxes = result.boxes
-        names = result.names
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        box_number = 1
+        # Prepare the multipart form data
+        files = {
+            'frame': ('frame.jpg', frame_bytes, 'image/jpeg')
+        }
 
+        # handled_balls = response.json()
         calculate = frame_count % frame_skip == 0
 
-        detected_balls = []
-        for box, cls, conf in zip(boxes.data, boxes.cls, boxes.conf):
-            if names[int(cls)] != 'ball':
-                continue
-            x1, y1, x2, y2 = box[:4].int().tolist()
-            ball_data = {
-                "x1": x1,
-                "y1": y1,
-                "x2": x2,
-                "y2": y2
-            }
-            detected_balls.append(ball_data)
-
-        payload = {
-            "frame": int(cap.get(cv2.CAP_PROP_POS_FRAMES)),
-            "balls": detected_balls
-        }
-        response = requests.post(backend_url, json=payload)
-        tracked_data = response.json()
-        tracked_balls = tracked_data["tracked_balls"]
-
         if calculate:
-            for box in tracked_balls:
-                ball_id = box["id"]
-                x1, y1, x2, y2 = [box["x1"], box["y1"], box["x2"], box["y2"]]
-                start = time.perf_counter()
-                stored = ball_id in color_store
-                color = color_store.get(ball_id, '?')
-                if not stored:
-                    color = predict_color(img, x1, y1, x2, y2)
-                    if color.endswith("_str") and ball_id is not None:
-                        color_store[ball_id] = color
+            # Send the frame to the FastAPI backend
+            response = requests.post(backend_url, files=files)
 
-                    box_number += 1
-                end = time.perf_counter()
+            # Check response
+            if response.status_code != 200:
+                print(f"Failed to send frame. Status code: {response.status_code}")
+                continue
+            handled_balls = response.json()
+            for box in handled_balls:
+                x1, y1, x2, y2 = [box["x1"], box["y1"], box["x2"], box["y2"]]
+                color = box["color"]
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
                 cv2.putText(frame, f"{color}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
 
-        cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
+            cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
 
         if not calculate:
             frame = prevFrame
